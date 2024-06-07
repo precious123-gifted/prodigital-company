ef4c794df838ac91401bf73b633612fc35760fb6
precious123-gifted <ogunameprecious123@gmail.com>
2024-06-07 15:42:21 +0100

    debugging

--- src/app/api/allProductsProcessedData/route.ts
+++ src/app/api/allProductsProcessedData/route.ts
@@ -21,7 +21,7 @@ export async function GET(request: NextRequest) {
 
 
 
-    allData = [...NewLaptops,...UsedLaptops,...Accessories];
+    allData = [...Accessories,...NewLaptops,...UsedLaptops,];
 
     
 

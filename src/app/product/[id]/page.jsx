192302a40442f0b91f1bf95caf1b122d7ec98cc1
precious123-gifted <ogunameprecious123@gmail.com>
2024-06-08 13:14:02 +0100

    redebugging

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -5,7 +5,7 @@ const isDevelopment = process.env.NODE_ENV === 'development' ;
 const baseUrl = isDevelopment
   ? `http://localhost:${process.env.PORT}`
   : "https://prodigital-company.vercel.app/";
-const url = `${baseUrl}/api/allProductsProcessedData`;
+const url = `${baseUrl}/api/productsProcessedData`;
   
   
 

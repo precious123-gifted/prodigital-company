6419a54f582b01c54723073b55688e7eeb63fef8
precious123-gifted <ogunameprecious123@gmail.com>
2024-06-08 11:41:35 +0100

    very very very intensed debugging

--- src/app/products/[category]/page.jsx
+++ src/app/products/[category]/page.jsx
@@ -14,7 +14,7 @@ export async function generateStaticParams(category) {
   await dbConnect()
 
   const categoryString = `${category}`;
-  const url = `${baseUrl}/api/productsProcessedData`;
+  const url = `${baseUrl}/api/${category}ProcessedData`;
   console.log(`this is the category: ${categoryString}`)
   console.log(`this is the url: ${url}`)
 

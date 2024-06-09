eaebac3d86b26a5ebab74436bf234bf1a3513252
precious123-gifted <ogunameprecious123@gmail.com>
2024-06-09 18:37:27 +0100

    [feat]:about page style

--- src/slices/About/index.tsx
+++ src/slices/About/index.tsx
@@ -1,3 +1,4 @@
+import Bounded from "@/app/components/Bounded";
 import { Content } from "@prismicio/client";
 import { SliceComponentProps } from "@prismicio/react";
 
@@ -11,12 +12,25 @@ export type AboutProps = SliceComponentProps<Content.AboutSlice>;
  */
 const About = ({ slice }: AboutProps): JSX.Element => {
   return (
-    <section
+    <Bounded
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+
+      className="pt-[2vw] portrait:pt-[6vw] portrait:sm:pt-[3vw] pb-5 rounded  text-[#384d4d] space-y-10  portrait:space-y-80 portrait:sm:space-y-40"
     >
-      Placeholder component for about (variation: {slice.variation}) Slices
-    </section>
+     
+
+<div className="content w-full py-4 px-5 flex flex-col items-center space-y-[2.5vw] portrait:space-y-[4vw]">
+
+<div className="header text-[2vw] portrait:sm:text-[4vw] portrait:text-[7vw] text-center">{slice.primary.header}</div>
+
+<div className="writeup text-[1.4vw] portrait:sm:text-[3vw] portrait:text-[5vw]  text-center leading-[2.5vw] portrait:leading-[8vw] portrait:sm:leading-[5vw]">{slice.primary.writeup}</div>
+
+</div>
+
+
+
+    </Bounded>
   );
 };
 

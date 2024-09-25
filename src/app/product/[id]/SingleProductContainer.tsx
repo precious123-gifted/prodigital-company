d28cff21e793573f67836710d5f6481972758387
precious123-gifted <ogunameprecious123@gmail.com>
2024-09-25 03:20:39 +0100

    Single Product Page Clean

--- src/app/product/[id]/SingleProductContainer.tsx
+++ src/app/product/[id]/SingleProductContainer.tsx
@@ -60,10 +60,12 @@ export default function SingleProductContainer({singleProduct}:any) {
 
 
   const productToAdd = singleProduct;
+
   const existingProductIndex = cartedProducts.findIndex(
            (item) => item._id === productToAdd._id
          );
-  const handleAddToCartedProducts = () => {
+
+  const handleAddingProductsToCartedProducts = () => {
     
            if (existingProductIndex !== -1) {
             setPopupType("warning")
@@ -161,7 +163,7 @@ const {popupMessage,setPopupMessage} = useStateContext()
     
   return (
 <Bounded>
-      <div className=" hairexpandedcontainer  text-[#384d4d] w-auto   flex flex-col  items-center text-center space-y-5  portrait:px-[8vw] py-[2vw] portrait:py-[8vw]">
+      <div className=" laptopexpandedcontainer  text-[#384d4d] w-auto   flex flex-col  items-center text-center space-y-5  portrait:px-[8vw] py-[2vw] portrait:py-[8vw]">
 <div className="exiticon    w-full flex justify-end ">
  
 <Image onClick={handleExitClick} src={exitIcon} alt="exit-icon" className="landscape:w-[2vw] portrait:w-[6vw] portrait:sm:w-[4vw]  object-cover cursor-pointer"/>
@@ -169,7 +171,7 @@ const {popupMessage,setPopupMessage} = useStateContext()
     
 <div className="content landscape:w-[55%] space-y-[10vw]">
 
-   <div className="hairContainer w-full   flex flex-col landscape:space-x-12  landscape:flex-row landscape:justify-between landscape:items-start items-center text-center portrait:space-y-[3vw] ">
+   <div className="laptopContainer w-full   flex flex-col landscape:space-x-12  landscape:flex-row landscape:justify-between landscape:items-start items-center text-center portrait:space-y-[3vw] ">
    <div className="imagecontainer w-full">
 
 <div className="complimentaryimages flex landscape:w-[24vw] portrait:w-full   portrait:sm:w-full justify-between mb-[1vw] portrait:mb-[4vw] ">
@@ -194,31 +196,31 @@ const {popupMessage,setPopupMessage} = useStateContext()
 
 </div>
 
-<div className="hairdetails text-left w-full">
+<div className="laptopdetails text-left w-full">
 
 <div className="space-y-[1vw]"> 
 <div className="laptopTitle cursor-pointer text-[2vw] portrait:text-[5vw] text-nowrap portrait:text-wrap"><div >{singleProduct.brandName}<span className="ml-1 text-[#4b6363] text-wrap">{singleProduct.title}</span></div></div>
 
-        <div className="hairDescription text-[1.5vw]   portrait:text-[4vw] portrait:sm:text-[3vw]  ">
+        <div className="laptopDescription text-[1.5vw]   portrait:text-[4vw] portrait:sm:text-[3vw]  ">
         {singleProduct?.shortDescription}
           </div>
-          <div className="hairDescription text-[1.5vw]   portrait:text-[4vw] portrait:sm:text-[3vw]  ">
+          <div className="laptopDescription text-[1.5vw]   portrait:text-[4vw] portrait:sm:text-[3vw]  ">
         {singleProduct?.fullDescription}
           </div>
-        <div className="hairPrize   font-medium text-green-900 text-[2vw]  portrait:text-[4vw]">
+        <div className="laptopPrize   font-medium text-green-900 text-[2vw]  portrait:text-[4vw]">
         {singleProduct.price.toLocaleString('en-NG', { style: 'currency', currency: 'NGN' })}
         </div>
 </div>
    
 
-      <div onClick={handleAddToCartedProducts} className="addtocartedProductsbtn portrait:hidden   mt-[4vw] px-10 py-2 bg-[#202c2c] cursor-pointer text-[#F5ECF1] hover:text-[#bbdabd] hover:bg-[#121f1f] duration-[1s] ease-in-out   text-[1.5vw] portrait:text-[6vw]  portrait:sm:text-[4vw] rounded-md text-center">{existingProductIndex === -1 &&<>Add to Cart</>}{existingProductIndex !== -1 &&<>Added to Cart</>}</div>
+      <div onClick={handleAddingProductsToCartedProducts} className="addtocartedProductsbtn portrait:hidden   mt-[4vw] px-10 py-2 bg-[#202c2c] cursor-pointer text-[#F5ECF1] hover:text-[#bbdabd] hover:bg-[#121f1f] duration-[1s] ease-in-out   text-[1.5vw] portrait:text-[6vw]  portrait:sm:text-[4vw] rounded-md text-center">{existingProductIndex === -1 &&<>Add to Cart</>}{existingProductIndex !== -1 &&<>Added to Cart</>}</div>
 
       </div>
 </div>
 
       
    
-      <div onClick={handleAddToCartedProducts} className="addtocartedProductsbtnMObile  landscape:hidden   px-10 py-2  bg-[#202c2c] cursor-pointer text-[#F5ECF1] hover:text-[#bbdabd] hover:bg-[#121f1f] duration-[1s] ease-in-out text-[1.5vw] portrait:text-[6vw]  portrait:sm:text-[4vw] rounded-md text-center">{existingProductIndex === -1 &&<>Add to Cart</>}{existingProductIndex !== -1 &&<>Added to Cart</>}</div>
+      <div onClick={handleAddingProductsToCartedProducts} className="addtocartedProductsbtnMObile  landscape:hidden   px-10 py-2  bg-[#202c2c] cursor-pointer text-[#F5ECF1] hover:text-[#bbdabd] hover:bg-[#121f1f] duration-[1s] ease-in-out text-[1.5vw] portrait:text-[6vw]  portrait:sm:text-[4vw] rounded-md text-center">{existingProductIndex === -1 &&<>Add to Cart</>}{existingProductIndex !== -1 &&<>Added to Cart</>}</div>
 
 </div>
 

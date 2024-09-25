b60cc2e5d739c71027145ca61cadc6bee7e429db
precious123-gifted <ogunameprecious123@gmail.com>
2024-09-25 03:40:44 +0100

    Admin Page Transition Animation Cleanup

--- src/app/admin/client.tsx
+++ src/app/admin/client.tsx
@@ -7,7 +7,7 @@ import { CldUploadWidget } from 'next-cloudinary';
 import { ToastContainer, toast } from "react-toastify";
 import imagePlaceholder from "../../../public/image-fill.png"
 import { useStateContext } from '@/StateManager';
-import TransitionLink from '../components/TransitionLink';
+import Link from 'next/link';
 
 
 
@@ -149,7 +149,7 @@ export default function AdminClientPage({allProducts}:any) {
       const [resource2, setResource2] = useState<CloudinaryImageInfo | null | string>(null);
       const [resource3, setResource3] = useState<CloudinaryImageInfo | null | string>(null);
 
-      
+
 function isCloudinaryInfo(value: string | CloudinaryImageInfo): value is CloudinaryImageInfo {
   return typeof value === 'object' && 'public_id' in value; 
 }
@@ -531,14 +531,14 @@ onSubmit={uploadProduct}
                landscape:hover:border-[#bad8d863] duration-[0.2s]  ease-in-out w-auto flex flex-col items-center text-start  space-y-1"
              >
                 <div className="flex flex-col items-start">
-               <TransitionLink  href={`admin/product/${product._id}`}>
+               <Link  href={`admin/product/${product._id}`}>
                  <div className="laptopImage cursor-pointer w-[12vw] portrait:w-[26vw] portrait:sm:w-[23vw] object-contain">
                    <Image alt='' src={`${product.productMainImage}`} className="rounded-lg " width={960} height={1280} />
                  </div>
-               </TransitionLink>
-               <TransitionLink href={`/product/${product._id}`}>
+               </Link>
+               <Link href={`/product/${product._id}`}>
                  <div className="laptopTitle w-[12vw] portrait:w-[26vw]  cursor-pointer text-[1.5vw] portrait:text-[5vw] text-nowrap portrait:text-wrap"><div >{product.brandName}<span className="ml-1 text-[#4b6363] text-wrap">{product.title}</span></div></div>
-               </TransitionLink>
+               </Link>
                <div className="laptopDescription w-[12vw] portrait:w-[26vw]  cursor-pointer text-[1.19vw]  portrait:text-[4vw] portrait:sm:text-[3vw]">{product.shortDescription}</div>
                <div className="laptopPrice w-[12vw] portrait:w-[26vw]  cursor-pointer font-medium text-green-900 portrait:text-[4vw]">{product.price.toLocaleString('en-NG', { style: 'currency', currency: 'NGN' })}</div>
              </div>
